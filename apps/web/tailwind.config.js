950c0d5d426377dbb792b19a956a971a668333f2
Nebulaz7 <jp0712002@gmail.com>
2025-07-16 23:35:54 +0100

    Refactor Tailwind CSS configuration and remove deprecated files

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        proxima: ['"Proxima Soft"', "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
