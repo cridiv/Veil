62b08be2cad1e1e248b5ea9e8ed867aa0f88b574
Nebulaz7 <jp0712002@gmail.com>
2025-07-17 01:03:45 +0100

    Implement code changes to enhance functionality and improve performance

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,13 @@
-import React from "react";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
 
-const page = () => {
+export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-3xl font-bold mb-4">Tailwind CSS and DaisyUI Test</h1>
-      <button className="btn btn-primary mb-4">Primary Button</button>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">DaisyUI Card</h2>
-          <p>This is a card component from DaisyUI!</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-accent">Get Started</button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <NavBar />
+      <Hero />
+      <Footer />
     </div>
   );
-};
-
-export default page;
+}
