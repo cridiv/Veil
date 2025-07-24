cf874d03ec1cba42d8f5af9027e6a183093741c0
Nebulaz7 <jp0712002@gmail.com>
2025-07-24 13:17:58 +0100

    Remove AnalyticsSection component and update AnalyticsPage to simplify the dashboard layout.

--- apps/web/app/dashboard/analytics/page.tsx
+++ apps/web/app/dashboard/analytics/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import React from "react";
-import AnalyticsSection from "../components/AnalyticsSection";
 
 export default function AnalyticsPage() {
-  return (
-    <div>
-      <AnalyticsSection />
-    </div>
-  );
+  return <div>page</div>;
 }
